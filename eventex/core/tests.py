498b0c7f7e257d7c4b2a3ee686c19928f9a9d3b1
anderson marques <andersonoanjo18@gmail.com>
2017-12-19 00:10:44 -0300

    Add tests to view home

--- eventex/core/tests.py
+++ eventex/core/tests.py
@@ -1,3 +1,16 @@
+"""initial core tests"""
 from django.test import TestCase
 
-# Create your tests here.
+
+class HomeTest(TestCase):
+    """Home view Test Case"""
+    def setUp(self):
+        self.response = self.client.get('/')
+
+    def test_get(self):
+        """GET / must return status code 200"""
+        self.assertEqual(200, self.response.status_code)
+    
+    def test_template_used(self):
+        """Must use index.html"""
+        self.assertTemplateUsed(self.response, 'index.html')
